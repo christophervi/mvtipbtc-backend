917e9acb8bdcb88251f021345e3dbd9216be9193
Christopher Chan Vi <cvi3@gatech.edu>
2025-06-07 01:13:15 -0700

    Initial commit

---
+++ src/main/java/edu/gatech/cc/scp/mvtipbtc/MvtipbtcBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.gatech.cc.scp.mvtipbtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvtipbtcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvtipbtcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/gatech/cc/scp/mvtipbtc/MvtipbtcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gatech.cc.scp.mvtipbtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvtipbtcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
